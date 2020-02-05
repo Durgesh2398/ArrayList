57d03af2ce8b8694ee13a64ee95ccdf7d6a217a7
Durgesh <2308durgesh@gmail.com>
2020-02-05 20:28:04 +0530

    Create SubArraySum.java

---
+++ SubArraySum.java
@@ -0,0 +1,39 @@
+package arrayListQuestion;
+import java.util.*;
+public class SubArraySum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SubArraySum ob= new SubArraySum();
+		Scanner sc =new Scanner(System.in);
+		System.out.println("enter the number of inputs");
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("enter the sum to find");
+		int sum=sc.nextInt();
+		ob.subarraysum(arr,n,sum);
+	}
+	int subarraysum(int arr[],int n,int sum) {
+		int csum=0;
+		for(int i=0;i<n;i++) {
+			csum=arr[i];
+			for(int j=i+1;j<=n;j++) {
+				if(csum==sum) {
+					int p=j-1;
+					System.out.println(i+"between"+p);
+					return 1;
+				}
+				if(csum>sum||j==n) {
+					break;
+				}
+				csum=csum+arr[j];
+			}
+		}
+		System.out.println("not found");
+		return 0;
+	}
+
+}
