6d4b0f7cc7b3c86c81471db8ce39e65f1f9d1f2d
Durgesh <2308durgesh@gmail.com>
2020-02-06 23:08:09 +0530

    Create ZigZagnumbers.java

---
+++ ZigZagnumbers.java
@@ -0,0 +1,39 @@
+package arrayListQuestion;
+import java.util.*;
+public class ZigZagnumbers {
+	public static void main(String[]  args) {
+		Scanner sc =new Scanner(System.in);
+		ZigZagnumbers ob = new ZigZagnumbers();
+		System.out.println("enter the number of inputs");
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		ob.output(arr,n);
+	}
+	static void output(int arr[],int n) {
+		boolean flag=true;
+        int temp =0; 
+        for (int i=0; i<=arr.length-2; i++) { 
+            if (flag){ 
+                if (arr[i] > arr[i+1]) { 
+                    temp  = arr[i]; 
+                    arr[i] = arr[i+1]; 
+                    arr[i+1] = temp; 
+                } 
+            } 
+            else{ 
+                if (arr[i] < arr[i+1]) {  
+                    temp = arr[i]; 
+                    arr[i] = arr[i+1]; 
+                    arr[i+1] = temp; 
+                } 
+            } 
+            flag = !flag;
+        }
+        for(int i=0;i<arr.length;i++) {
+        	System.out.print(arr[i]+" ");
+        }
+	}
+}
